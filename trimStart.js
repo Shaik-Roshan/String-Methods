236456e5bf3f13636ab655b347b7d137a4fc32a5
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2023-01-20 20:29:40 +0530

    Added

---
+++ trimStart.js
@@ -0,0 +1,7 @@
+var greeting = "      good morning";
+
+console.log(greeting.length); //18
+
+greeting = greeting.trimStart();
+console.log(greeting.length); //12
+console.log(greeting); //good morning

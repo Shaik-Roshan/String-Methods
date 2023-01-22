d4fd5c3d5829a80cd336d230a1c92e617a6e57f1
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2023-01-22 10:01:22 +0530

    Added

---
+++ trim.js
@@ -0,0 +1,8 @@
+var friend='    yaseen    ';
+console.log(friend);//'    yaseen    '
+console.log(friend.length);//14
+
+friend=friend.trim();
+
+console.log(friend);//'yaseen'
+console.log(friend.length);//6

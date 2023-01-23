79a16cbfab88f6c1ebd0aeb95df6c8cb792f4338
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2023-01-23 22:42:49 +0530

    .\toUpperCase.js

---
+++ toUpperCase.js
@@ -0,0 +1,5 @@
+var x="to become a good developer practice more";
+console.log(x);//to become a good developer practice more
+
+x=x.toUpperCase();
+console.log(x)//TO BECOME A GOOD DEVELOPER PRACTICE MORE

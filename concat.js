1e5d22b6c8a3aca880047905710564d7bcce010b
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2023-01-27 22:09:33 +0530

    String Methods

---
+++ concat.js
@@ -0,0 +1,7 @@
+
+let str = "Roshan ";
+console.log(str.concat("brother how are you"), "and how is your health", "okay byee take care")
+
+let fname=" shaik mohammad";
+let lname="yaseen";
+console.log("hey my name is".concat(fname," ", lname))

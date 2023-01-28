8433de8037d89488617a5499af45bb5ffa7713b7
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2023-01-28 20:47:24 +0530

    String Method

---
+++ indexOf.js
@@ -0,0 +1,6 @@
+var car=["maruthi","hyundai","mahindra","toyota"];
+console.log(car.indexOf("mahindra"));//2
+console.log(car.indexOf("maruthi"));//0
+
+var x="hello world";
+console.log(x.indexOf('e'))//1

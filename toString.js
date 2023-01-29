7037408852e2ef8bb07671ff935d6342b8a92811
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2023-01-29 23:14:32 +0530

    String Method

---
+++ toString.js
@@ -0,0 +1,8 @@
+var x= new String("Hello World!");
+
+var y=console.log(x.toString());//Hello World!
+
+//converting number to string
+var a=100;
+var b=a.toString();
+console.log(typeof(b));//string
